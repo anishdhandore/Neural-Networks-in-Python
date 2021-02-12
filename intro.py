2dbd3ae03f3be3b3401dc06de1335e2b902bfe26
Anish <anish.dhandore@gmail.com>
2021-02-12 14:50:08 +0530

    introduction to NN

---
+++ intro.py
@@ -0,0 +1,24 @@
+# import sys
+# import numpy as np
+# import matplotlib
+
+# print(np.__version__)
+# print(matplotlib.__version__)
+
+
+inputs = [1.1, 5.1, 8.9]
+# every input has a unique weight
+weights = [3.5, 4.7, 2.3]
+bias = 3
+
+output = (inputs[0]*weights[0]) + (inputs[1]*weights[1]) + (inputs[2]*weights[2]) + bias
+
+print(output)
+  
+
+
+
+
+
+
+
