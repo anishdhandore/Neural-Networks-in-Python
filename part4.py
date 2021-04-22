5fd7f23ad719289b46d48af559436a2abbfed79f
Anish <anish.dhandore@gmail.com>
2021-04-23 03:45:46 +0530

    used classes to work through NEURON INPUTS

--- part4.py
+++ part4.py
@@ -6,33 +6,27 @@
 # BATCH SIZE INCREASES, THE LINE (line that fits through the samples) WIGGLES MUCH LESS THAN BEFORE
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]  # values from input layer, vectors
+np.random.seed(0)
 
-# we need to TRANSPOSE THE WEIGHTS IN ORDER TO AVOOID THE SHAPE ERROR
-# TO AVOID SHAPE ERROR : MAKE SURE ROWS(INPUTS) == COLUMNS(WEIGHTS)
-# SO TRANSPOSE THE WEIGHTS TO DO THE DOT PRODUCT!
-weights1 = [ [0.2, 0.8, -0.5, 1.0], 
-            [0.5, -0.91, 0.26, -0.5], 
-            [-0.26, -0.27, 0.17, 0.87]]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-biases1 = [2, 3, 0.5]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-weights2 = [[0.1, -0.14, 0.5], 
-            [-0.5, 0.12, -0.33], 
-            [-0.44, 0.73, -0.13]]
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
 
-biases2 = [-1, 2, -0.5]
+layer1.forward(X)
+layer2.forward(layer1.output)
+# print(layer1.output)
+print(layer2.output)
 
-weights3 = [ [0.2, 0.8, -0.5, 1.0], 
-            [0.5, -0.91, 0.26, -0.5], 
-            [-0.26, -0.27, 0.17, 0.87]]
 
-biases3 = [2, 3, 0.5]
 
-# array function is just used to implement the transpose function on weights
-layer1_output = np.dot(inputs, np.array(weights1).transpose()) + biases1
-layer2_output = np.dot(layer1_output, np.array(weights2).transpose()) + biases2
-print(layer2_output) 
 
