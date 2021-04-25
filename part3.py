dfd8d0b9986a178d79eae767dc5a1f4e9d4e6647
Anish <anish.dhandore@gmail.com>
2021-04-26 03:59:16 +0530

    single layer of neurons

--- part3.py
+++ part3.py
@@ -9,7 +9,7 @@ weights = [ [0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
+output = np.dot(inputs, np.array(weights).transpose()) + biases
 print(output)
 
 ''' for loop
