7af083988f9612075d3cd68b4892ab8e5d5481d8
Anish <anish.dhandore@gmail.com>
2021-05-23 00:17:25 +0530

    modified class object

--- part4.py
+++ part4.py
@@ -6,26 +6,30 @@
 # BATCH SIZE INCREASES, THE LINE (line that fits through the samples) WIGGLES MUCH LESS THAN BEFORE
 import numpy as np
 
-np.random.seed(0)
-
+ # X are the input values, NOT the number of inputs
 X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]]
+
+np.random.seed(0)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)  # weight of each neuron, here: 4 inputs from X
         self.biases = np.zeros((1, n_neurons))
+
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
 
 layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+layer2= Layer_Dense(5,2)
 
-layer1.forward(X)
-layer2.forward(layer1.output)
-# print(layer1.output)
-print(layer2.output)
+layer1_output = layer1.forward(X)
+layer2_output = layer2.forward(layer1_output)
+
+print(layer1_output)
+print(layer2_output)
 
 
 
